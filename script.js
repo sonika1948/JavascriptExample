e6ae43e87f1386bc7a1b5371c4f2ef4b209c3216
sonika1948 <155156742+sonika1948@users.noreply.github.com>
2023-12-29 19:24:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("askForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    // You can customize this alert message
+    alert("Great! Looking forward to it!");
+});
